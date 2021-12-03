133bf7428a74abc0f831bd754f6523127691bd1f
JERRY <xulx@qtec.cn>
2021-12-03 16:21:33 +0800

    token拦截

--- Source/unicom/UnicomServer/src/main/java/com/qtec/unicom/shiro/ShiroConfig.java
+++ Source/unicom/UnicomServer/src/main/java/com/qtec/unicom/shiro/ShiroConfig.java
@@ -62,7 +62,7 @@ public class ShiroConfig {
 
         //其余接口一律拦截
         //主要这行代码必须放在所有权限设置的最后，不然会导致所有 url 都被拦截
-//        filterChainDefinitionMap.put("/**", "jwt");
+        filterChainDefinitionMap.put("/**", "jwt");
 
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
         System.out.println("Shiro拦截器工厂类注入成功");
