39f20e7ae1efc3f7dbd302fcbcb391301fc315e1
JERRY <xulx@qtec.cn>
2021-12-04 13:49:14 +0800

    添加qkd配置

---
+++ Source/unicom/Common/src/main/java/com/qtec/unicom/controller/vo/QKDConfig.java
@@ -0,0 +1,18 @@
+package com.qtec.unicom.controller.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class QKDConfig implements Serializable {
+    @ApiModelProperty(value = "本端用户名",notes = "本端用户名")
+    private String localName;
+    @ApiModelProperty(value = "对端用户名",notes = "对端用户名")
+    private String peerName;
+    @ApiModelProperty(value = "设备密钥",notes = "设备密钥")
+    private String devKey;
+    @ApiModelProperty(value = "加密密钥",notes = "加密密钥")
+    private String cryptKey;
+}
