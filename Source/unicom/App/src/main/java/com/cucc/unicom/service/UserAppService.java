77221ff627f4ba222b8b7454fb43376132c4fde5
jerry <xulx@qtec.cn>
2022-01-20 14:12:17 +0800

    应用管理员所管理应用返回数据修改

--- Source/unicom/App/src/main/java/com/cucc/unicom/service/UserAppService.java
+++ Source/unicom/App/src/main/java/com/cucc/unicom/service/UserAppService.java
@@ -3,7 +3,6 @@ package com.cucc.unicom.service;
 import com.cucc.unicom.controller.vo.UserAppRequest;
 import com.cucc.unicom.pojo.App;
 import com.cucc.unicom.pojo.dto.CurrentAppManager;
-import com.cucc.unicom.pojo.dto.CurrentManagerApp;
 
 import java.util.List;
 
