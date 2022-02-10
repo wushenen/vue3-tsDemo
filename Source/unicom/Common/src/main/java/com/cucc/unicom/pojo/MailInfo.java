2cdf208d128a15ffaefaafdb34c7619b2f287701
JERRY <xulx@qtec.cn>
2022-02-10 09:39:23 +0800

    优化

--- Source/unicom/Common/src/main/java/com/cucc/unicom/pojo/MailInfo.java
+++ Source/unicom/Common/src/main/java/com/cucc/unicom/pojo/MailInfo.java
@@ -1,6 +1,10 @@
 package com.cucc.unicom.pojo;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class MailInfo {
 
     private String subject;
@@ -8,33 +12,6 @@ public class MailInfo {
     private String destination;
 
     private String text;
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public MailInfo() {
-    }
 
     public MailInfo(String subject, String destination, String text) {
         this.subject = subject;
